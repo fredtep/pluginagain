ee02239535438aec4dab7aad702053d2d6eb786d
Fr3sh <28731248+fredtep@users.noreply.github.com>
2023-11-25 10:55:40 +0100

    Update and rename a2.js to a3.js

--- a3.js
+++ a3.js
@@ -3,7 +3,7 @@ const url = 'https://127.0.0.1:5000/home';
 // Making a GET request using fetch()
 fetch(url, {
     method: "GET", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, *cors, same-origin
+    mode: "same-origin", // no-cors, *cors, same-origin
     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
     credentials: "same-origin", // include, *same-origin, omit  
 })

